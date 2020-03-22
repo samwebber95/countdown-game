da5f652630da343151ca6d44d4d5d45427c21acb
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-03-22 19:41:00 +0000

    2

--- src/Display/index.js
+++ src/Display/index.js
@@ -4,13 +4,18 @@ function Display({ letters, setLetters }) {
   function getVowel() {
     const vowels = ["a", "e", "i", "o", "u"];
     const randomVowel = Math.floor(Math.random() * vowels.length);
-    return setLetters(...vowels, vowels[randomVowel]);
+    const vowel = vowels[randomVowel];
+    renderLetter(vowel);
+  }
+
+  function renderLetter(vowel) {
+    setLetters([...letters, vowel]);
   }
 
   return (
     <div>
       <h1>Welcome to COUNTDOWN</h1>
-      <h2>{letters}</h2>
+      <p>{letters}</p>
       <button onClick={getVowel}>Vowel</button>
       <button>Consenant</button>
     </div>
