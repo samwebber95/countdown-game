de0d9c55632021005244c6d8232f853ad9d83881
Samuel Webber <samuelwebber@Samuels-MacBook-Pro.local>
2020-03-28 18:11:49 +0000

    rachel riley stuff

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import "./App.css";
 import Display from "../Display/index";
 import Answers from "../Answers/index";
+import RachelRiley from "../RachelRiley/index";
 
 function App() {
   const [letters, setLetters] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const [answers, setAnswers] = useState([]);
 
   return (
     <div className="App">
       <Display letters={letters} setLetters={setLetters} />
-      <Answers />
+      <Answers
+        answer={answer}
+        answers={answers}
+        setAnswer={setAnswer}
+        setAnswers={setAnswers}
+      />
+      <RachelRiley letters={letters} />
     </div>
   );
 }
---
+++ src/RachelRiley/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import css from "./index.module.css";
+
+function RachelRiley({ letters }) {
+  function setSpeech() {
+    console.log(`you chose ${letters}`);
+  }
+
+  setSpeech();
+
+  return (
+    <div>
+      <img
+        className={css.hostImage}
+        src="https://fanclub.org.uk/wpimages/wp2fe9c22b_06.png"
+        alt="Rachel Riley"
+      ></img>
+
+      <img
+        className={css.hostSpeechBubble}
+        src="https://clipartmag.com/images/empty-speech-bubble-5.png"
+        alt="speech"
+      ></img>
+    </div>
+  );
+}
+
+export default RachelRiley;
